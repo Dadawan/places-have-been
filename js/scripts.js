d5bf2740211b427e8c38b0042ab18fd74c43b33a
Dadawan <dudethatsmeta@outlook.com>
2017-03-16 22:46:06 -0500

    Add multiple properties for place constructor

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//business logic
+//location, landmarks, time of year, notes.
+function Place(location, landmarks, date, rating, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.date = date;
+  this.rating = rating;
+  this.notes = notes;
+}
+
+//user interface logic
